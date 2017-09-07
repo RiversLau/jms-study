55afe1dee302446a1b081755d9ba397fdd2d9dd8
Rivers <zhaoxiang0805@hotmail.com>
2017-09-07 16:50:27 +0800

    rocketmq filter message

--- src/main/java/com/zhaoxiang/rocketmq/quickStart/Producer.java
+++ src/main/java/com/zhaoxiang/rocketmq/quickStart/Producer.java
@@ -4,9 +4,13 @@ import com.zhaoxiang.rocketmq.RocketMQConstants;
 import org.apache.rocketmq.client.exception.MQBrokerException;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.MessageQueueSelector;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageQueue;
 import org.apache.rocketmq.remoting.exception.RemotingException;
+
+import java.util.List;
 
 /**
  * Author: RiversLau
