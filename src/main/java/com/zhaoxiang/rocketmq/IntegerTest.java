86c6c90760f8444e4f08103fab8063bc74d6c5f6
Rivers <zhaoxiang0805@hotmail.com>
2017-09-12 16:25:47 +0800

    rocketmq filter message

---
+++ src/main/java/com/zhaoxiang/rocketmq/IntegerTest.java
@@ -0,0 +1,15 @@
+package com.zhaoxiang.rocketmq;
+
+/**
+ * Author: RiversLau
+ * Date: 2017/9/12 14:46
+ */
+public class IntegerTest {
+
+    public static void main(String[] args) {
+
+        Integer t1 = -128;
+        Integer t2 = -128;
+        System.out.println(t1 == t2);
+    }
+}
